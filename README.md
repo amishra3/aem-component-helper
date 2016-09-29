5744704bfc07737e9cb7e4315e4c3bbec2a758e5
sljones1 <stephen.jones@alumni.colostate.edu>
2016-09-28 22:36:45 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,45 @@
+## AEM Component CLI
+This Tool was designed to create AEM Skeleton Components via the command line.
+It allows for a lot of different options for the Skeleton Component.
+### Basic Usage
+This command
+`aem-component -n C01-headline -g test -l`
+Creates this structure
+```
+C01-headline
+|-- _cq_dialog.xml
+|-- .content.xml
+|-- C01-headline.html
+|-- clientlib
+    |--.content.xml
+    |--css.txt
+    |--js.txt
+    |--js
+    |--css
+```
+Here is the full list of options:
+```
+aem-component -h
+
+Usage: index [options]
+
+Options:
+
+  -h, --help           output usage information
+  -V, --version        output the version number
+  -v, --verbose        verbose output
+  -n, --name <s>       set name (required)
+  -g, --group <s>      set component group (defaults to .hidden)
+  -t, --title [s]      set title (optional)
+  -d, --directory [s]  root directory (optional)
+  -c, --classic        create classic dialog
+  -l, --clientlib     create component clientlib
+```
+### Config file
+There is an option to create an `aem.config` file.
+Right now you can specify the directory and group
+Example:
+```
+directory = /apps/project/components/modular/
+group = company
+```
---
+++ bin/component.js
@@ -0,0 +1,135 @@
+#!/usr/bin/env node
+"use strict";
+const fs = require('fs');
+let program = require('commander');
+var properties = require ("properties");
+program
+  .version('0.5.0')
+  .option('-v, --verbose', 'verbose output')
+  .option('-n, --name <s>', 'set name (required)')
+  .option('-g, --group [s]', 'set component group (defaults to .hidden)')
+  .option('-t, --title [s]', 'set title (optional)')
+  .option('-d, --directory [s]', 'root directory (optional)')
+  .option('-c, --classic', 'create classic dialog')
+  .option('-l, --clientlib', 'create component client-lib')
+  .parse(process.argv);
+let path = program.name;
+// Templates
+let xml = `<?xml version="1.0" encoding="UTF-8"?>
+  <jcr:root xmlns:sling="http://sling.apache.org/jcr/sling/1.0" xmlns:cq="http://www.day.com/jcr/cq/1.0" xmlns:jcr="http://www.jcp.org/jcr/1.0"
+    jcr:description="${program.title || program.name}"
+    jcr:primaryType="cq:Component"
+    jcr:title="${program.title || program.name}"
+    sling:resourceSuperType="foundation/components/parbase"
+    allowedParents="[*/parsys]"
+    componentGroup="${program.group || `.hidden`}"/>`;
+let dialog = `<?xml version="1.0" encoding="UTF-8"?>
+  <jcr:root xmlns:sling="http://sling.apache.org/jcr/sling/1.0" xmlns:cq="http://www.day.com/jcr/cq/1.0" xmlns:jcr="http://www.jcp.org/jcr/1.0" xmlns:nt="http://www.jcp.org/jcr/nt/1.0"
+      jcr:primaryType="nt:unstructured"
+      jcr:title="${program.title || program.name}"
+      sling:resourceType="cq/gui/components/authoring/dialog">
+      <content
+          jcr:primaryType="nt:unstructured"
+          sling:resourceType="granite/ui/components/foundation/container">
+          <layout
+              jcr:primaryType="nt:unstructured"
+              sling:resourceType="granite/ui/components/foundation/layouts/tabs"
+              type="nav"/>
+          <items jcr:primaryType="nt:unstructured">
+              <${program.name}
+                  jcr:primaryType="nt:unstructured"
+                  jcr:title="${program.title || program.name} Properties"
+                  sling:resourceType="granite/ui/components/foundation/section">
+                  <layout
+                      jcr:primaryType="nt:unstructured"
+                      sling:resourceType="granite/ui/components/foundation/layouts/fixedcolumns"/>
+                  <items jcr:primaryType="nt:unstructured">
+                  </items>
+              </${program.name}>
+          </items>
+      </content>
+  </jcr:root>`;
+
+let classic = `<?xml version="1.0" encoding="UTF-8"?>
+<jcr:root xmlns:cq="http://www.day.com/jcr/cq/1.0" xmlns:jcr="http://www.jcp.org/jcr/1.0" xmlns:nt="http://www.jcp.org/jcr/nt/1.0"
+    jcr:primaryType="cq:Dialog"
+    title="dialog"
+    xtype="dialog">
+    <items jcr:primaryType="cq:WidgetCollection">
+        <${program.name}
+            jcr:primaryType="cq:Widget"
+            title="${program.title || program.name}"
+            xtype="panel">
+            <items jcr:primaryType="cq:WidgetCollection">
+            </items>
+        </${program.name}>
+    </items>
+</jcr:root>`;
+let clientlib = `<?xml version="1.0" encoding="UTF-8"?>
+<jcr:root xmlns:cq="http://www.day.com/jcr/cq/1.0" xmlns:jcr="http://www.jcp.org/jcr/1.0"
+    jcr:primaryType="cq:ClientLibraryFolder"
+    categories="[${program.name}]"/>`;
+let html = `<div><div>`;
+// Function to write to a file
+let write = (file,text) => {
+  fs.writeFile(file, text, (err) => {
+    if (err) throw err;
+    if(program.verbose) console.log(`${file} is saved!`);
+  });
+};
+// Main Function
+let create = () => {
+  write(`${path}/.content.xml`,xml);
+  // Decides on dialog
+  if(program.classic) {
+    write(`${path}/dialog.xml`,classic);
+  } else {
+    write(`${path}/_cq_dialog.xml`,dialog);
+  }
+  write(`${path}/${program.name}.html`,html);
+  // If clientlib, will write dummy clientlib.
+  if(program.clientlib) {
+    let clientlibFolder = `${path}/clientlib/`;
+    fs.mkdir(clientlibFolder,() => {
+      let createClientLib = (type) => {
+        let name = `${clientlibFolder}${type}`;
+        fs.mkdirSync(name);
+        write(`${name}.txt`,`#base=${type}`);
+      };
+      write(`${clientlibFolder}.content.xml`,clientlib);
+      createClientLib('js');
+      createClientLib('css');
+    });
+  }
+};
+// Checks Values
+let check = () => {
+  if(!program.name) {
+    console.log(`Error: you have not specified the component name.`);
+    program.help();
+  }
+
+  if(program.directory){
+    if(!program.directory.endsWith('/')) program.directory += '/';
+    path = program.directory + program.name;
+  }
+  fs.mkdir(path,create);
+};
+// Processes Arguments
+fs.access("aem.config", fs.F_OK, function(err) {
+  if (!err) {
+    properties.parse("aem.config", { path: true }, function (error, obj){
+      if (error) return console.error (error);
+      program.directory = (program.directory || obj.directory);
+      program.group = (program.group || obj.group);
+      check();
+    });
+  } else {
+    if(err.code === 'ENOENT') {
+      check();
+    } else {
+      return console.error(err);
+    }
+
+  }
+});
