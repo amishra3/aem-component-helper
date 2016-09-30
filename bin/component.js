ca31fc35e02c74c8ce0f340330106084a1b07249
sljones1 <stephen.jones@alumni.colostate.edu>
2016-09-29 21:00:16 -0600

    remove properties file and fix bugs (#3)

--- bin/component.js
+++ bin/component.js
@@ -2,7 +2,6 @@
 "use strict";
 const fs = require('fs');
 let program = require('commander');
-var properties = require ("properties");
 program
   .version('0.8.0')
   .option('-v, --verbose', 'verbose output')
@@ -14,65 +13,6 @@ program
   .option('-l, --clientlib', 'create component client-lib')
   .parse(process.argv);
 let path = program.name;
-// Templates
-let xml = (item) => `<?xml version="1.0" encoding="UTF-8"?>
-  <jcr:root xmlns:sling="http://sling.apache.org/jcr/sling/1.0" xmlns:cq="http://www.day.com/jcr/cq/1.0" xmlns:jcr="http://www.jcp.org/jcr/1.0"
-    jcr:description="${item.title || item.name}"
-    jcr:primaryType="cq:Component"
-    jcr:title="${item.title || item.name}"
-    sling:resourceSuperType="foundation/components/parbase"
-    allowedParents="[*/parsys]"
-    componentGroup="${item.group || `.hidden`}"/>`;
-let dialog = `<?xml version="1.0" encoding="UTF-8"?>
-  <jcr:root xmlns:sling="http://sling.apache.org/jcr/sling/1.0" xmlns:cq="http://www.day.com/jcr/cq/1.0" xmlns:jcr="http://www.jcp.org/jcr/1.0" xmlns:nt="http://www.jcp.org/jcr/nt/1.0"
-      jcr:primaryType="nt:unstructured"
-      jcr:title="${program.title || program.name}"
-      sling:resourceType="cq/gui/components/authoring/dialog">
-      <content
-          jcr:primaryType="nt:unstructured"
-          sling:resourceType="granite/ui/components/foundation/container">
-          <layout
-              jcr:primaryType="nt:unstructured"
-              sling:resourceType="granite/ui/components/foundation/layouts/tabs"
-              type="nav"/>
-          <items jcr:primaryType="nt:unstructured">
-              <${program.name}
-                  jcr:primaryType="nt:unstructured"
-                  jcr:title="${program.title || program.name} Properties"
-                  sling:resourceType="granite/ui/components/foundation/section">
-                  <layout
-                      jcr:primaryType="nt:unstructured"
-                      sling:resourceType="granite/ui/components/foundation/layouts/fixedcolumns"/>
-                  <items jcr:primaryType="nt:unstructured">
-                  </items>
-              </${program.name}>
-          </items>
-      </content>
-  </jcr:root>`;
-let cqEdit = `<?xml version="1.0" encoding="UTF-8"?>
-<jcr:root xmlns:cq="http://www.day.com/jcr/cq/1.0" xmlns:jcr="http://www.jcp.org/jcr/1.0"
-  jcr:primaryType="cq:EditConfig">
-</jcr:root>`;
-let classic = `<?xml version="1.0" encoding="UTF-8"?>
-<jcr:root xmlns:cq="http://www.day.com/jcr/cq/1.0" xmlns:jcr="http://www.jcp.org/jcr/1.0" xmlns:nt="http://www.jcp.org/jcr/nt/1.0"
-    jcr:primaryType="cq:Dialog"
-    title="dialog"
-    xtype="dialog">
-    <items jcr:primaryType="cq:WidgetCollection">
-        <${program.name}
-            jcr:primaryType="cq:Widget"
-            title="${program.title || program.name}"
-            xtype="panel">
-            <items jcr:primaryType="cq:WidgetCollection">
-            </items>
-        </${program.name}>
-    </items>
-</jcr:root>`;
-let clientlib = `<?xml version="1.0" encoding="UTF-8"?>
-<jcr:root xmlns:cq="http://www.day.com/jcr/cq/1.0" xmlns:jcr="http://www.jcp.org/jcr/1.0"
-    jcr:primaryType="cq:ClientLibraryFolder"
-    categories="[${program.name}]"/>`;
-let html = `<div></div>`;
 // Function to write to a file
 let write = (file,text) => {
   fs.writeFile(file, text, (err) => {
@@ -86,9 +26,9 @@ let create = () => {
   write(`${path}/_cq_editConfig.xml`,cqEdit);
   // Decides on dialog
   if(program.classic) {
-    write(`${path}/dialog.xml`,classic);
+    write(`${path}/dialog.xml`,classic(program));
   } else {
-    write(`${path}/_cq_dialog.xml`,dialog);
+    write(`${path}/_cq_dialog.xml`,dialog(program));
   }
   write(`${path}/${program.name}.html`,html);
   // If clientlib, will write dummy clientlib.
@@ -100,7 +40,7 @@ let create = () => {
         fs.mkdirSync(name);
         write(`${name}.txt`,`#base=${type}`);
       };
-      write(`${clientlibFolder}.content.xml`,clientlib);
+      write(`${clientlibFolder}.content.xml`,clientlib(program));
       createClientLib('js');
       createClientLib('css');
     });
@@ -108,7 +48,7 @@ let create = () => {
 };
 // Checks Values
 let check = () => {
-  if(!program.name) {
+  if(!program.name || typeof program.name != "string") {
     console.log(`Error: you have not specified the component name.`);
     program.help();
   }
@@ -119,21 +59,64 @@ let check = () => {
   }
   fs.mkdir(path,create);
 };
-// Processes Arguments
-fs.access("aem.config", fs.F_OK, function(err) {
-  if (!err) {
-    properties.parse("aem.config", { path: true }, function (error, obj){
-      if (error) return console.error (error);
-      program.directory = (program.directory || obj.directory);
-      program.group = (program.group || obj.group);
-      check();
-    });
-  } else {
-    if(err.code === 'ENOENT') {
-      check();
-    } else {
-      return console.error(err);
-    }
+check();
 
-  }
-});
+// Templates
+let xml = (item) => `<?xml version="1.0" encoding="UTF-8"?>
+  <jcr:root xmlns:sling="http://sling.apache.org/jcr/sling/1.0" xmlns:cq="http://www.day.com/jcr/cq/1.0" xmlns:jcr="http://www.jcp.org/jcr/1.0"
+    jcr:description="${item.title || item.name}"
+    jcr:primaryType="cq:Component"
+    jcr:title="${item.title || item.name}"
+    sling:resourceSuperType="foundation/components/parbase"
+    allowedParents="[*/parsys]"
+    componentGroup="${item.group || `.hidden`}"/>`;
+let dialog = (item) => `<?xml version="1.0" encoding="UTF-8"?>
+  <jcr:root xmlns:sling="http://sling.apache.org/jcr/sling/1.0" xmlns:cq="http://www.day.com/jcr/cq/1.0" xmlns:jcr="http://www.jcp.org/jcr/1.0" xmlns:nt="http://www.jcp.org/jcr/nt/1.0"
+      jcr:primaryType="nt:unstructured"
+      jcr:title="${item.title || item.name}"
+      sling:resourceType="cq/gui/components/authoring/dialog">
+      <content
+          jcr:primaryType="nt:unstructured"
+          sling:resourceType="granite/ui/components/foundation/container">
+          <layout
+              jcr:primaryType="nt:unstructured"
+              sling:resourceType="granite/ui/components/foundation/layouts/tabs"
+              type="nav"/>
+          <items jcr:primaryType="nt:unstructured">
+              <${item.name}
+                  jcr:primaryType="nt:unstructured"
+                  jcr:title="${item.title || item.name} Properties"
+                  sling:resourceType="granite/ui/components/foundation/section">
+                  <layout
+                      jcr:primaryType="nt:unstructured"
+                      sling:resourceType="granite/ui/components/foundation/layouts/fixedcolumns"/>
+                  <items jcr:primaryType="nt:unstructured">
+                  </items>
+              </${item.name}>
+          </items>
+      </content>
+  </jcr:root>`;
+let cqEdit = `<?xml version="1.0" encoding="UTF-8"?>
+<jcr:root xmlns:cq="http://www.day.com/jcr/cq/1.0" xmlns:jcr="http://www.jcp.org/jcr/1.0"
+  jcr:primaryType="cq:EditConfig">
+</jcr:root>`;
+let classic = (item) => `<?xml version="1.0" encoding="UTF-8"?>
+<jcr:root xmlns:cq="http://www.day.com/jcr/cq/1.0" xmlns:jcr="http://www.jcp.org/jcr/1.0" xmlns:nt="http://www.jcp.org/jcr/nt/1.0"
+    jcr:primaryType="cq:Dialog"
+    title="dialog"
+    xtype="dialog">
+    <items jcr:primaryType="cq:WidgetCollection">
+        <${item.name}
+            jcr:primaryType="cq:Widget"
+            title="${item.title || item.name}"
+            xtype="panel">
+            <items jcr:primaryType="cq:WidgetCollection">
+            </items>
+        </${item.name}>
+    </items>
+</jcr:root>`;
+let clientlib = (item) => `<?xml version="1.0" encoding="UTF-8"?>
+<jcr:root xmlns:cq="http://www.day.com/jcr/cq/1.0" xmlns:jcr="http://www.jcp.org/jcr/1.0"
+    jcr:primaryType="cq:ClientLibraryFolder"
+    categories="[${item.name}]"/>`;
+let html = `<div></div>`;
